0ddbb9ac628d0387ee51326e1ad543cac133063d
Tyrone <steven.orrego93@gmail.com>
2021-10-21 17:09:19 -0500

    framework and scaffolding of the app

--- src/pages/Home/Home.styles.tsx
+++ src/pages/Home/Home.styles.tsx
@@ -7,10 +7,18 @@ export const Container = styled.div`
  justify-content: center;
  margin: 0 auto;
  min-height: 100vh;
+ text-align: center;
 `;
 
-export const Content = styled.div` 
-background-color: #ffffff white;
-width: 500px;
-
+export const Content = styled.div`
+ background-color: white;
+ border-radius: 12px;
+ display: grid;
+ grid-gap: 40px;
+ height: 100vh;
+ padding: 32px;
+ width: 100%;
+ @media (min-width: 600px) {
+    width: 500px;
+  }
 `;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,14 @@
-import {Container} from "./Home.styles";
-export const Home = () => {
-    return <Container>home</Container>
+import { Container, Content } from "./Home.styles";
 
+
+export const Home = () => {
+    return (
+        <Container>
+            <Content>
+                <h1> Hola </h1>
+
+                <h2>7+6</h2>
+            </Content>
+        </Container>
+    );
 };
