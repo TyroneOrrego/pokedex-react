a3541ed671748d01b07aec0fb9353cbeed3676fe
Tyrone <steven.orrego93@gmail.com>
2021-11-18 18:39:30 -0500

    Skeleton

--- src/pages/Pokedex/Pokedex.tsx
+++ src/pages/Pokedex/Pokedex.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { IoIosArrowRoundBack, IoIosMenu } from "react-icons/io";
-
+import Skeletons from "react-loading-skeleton";
 import { Card } from "../../components/Card";
 
 import { usePokemons } from "../../Hooks/usePokemons";
@@ -10,13 +10,14 @@ import {
   PokemonsList,
   Content,
   Header,
+  SkeletonsContainer,
   Image,
   PokeWrapper,
   PokemonName,
   Title,
 } from "./Pokedex.styles";
 export const Pokedex = () => {
-  const { pokemons = [] } = usePokemons();
+  const { pokemons, loading = [] } = usePokemons();
   return (
     <Container>
       <Content>
@@ -29,6 +30,9 @@ export const Pokedex = () => {
           </Header>
           <Title>Pokedex</Title>
         </div>
+        <SkeletonsContainer>
+          {loading && <Skeletons count={10} style={{ height: 100 }} />}
+        </SkeletonsContainer>
         <PokemonsList>
           {pokemons.map((pokemon) => (
             <Card
